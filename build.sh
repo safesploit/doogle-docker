302552cf650f8cd6a1663586e38d4976a02664ac
Zepher Ashe <safesploit@users.noreply.github.com>
2023-02-25 12:09:00 +0000

    Add files via upload

--- build.sh
+++ build.sh
@@ -4,6 +4,15 @@ set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # 
+if [ -d "$SCRIPT_DIR/doogle"]
+then
+	echo "Doogle Directory exists, exiting!"
+	exit 1
+fi
+# if [ -d "$SCRIPT_DIR/src"]
+# 	echo "src Directory exists, exiting!"
+# 	exit 1
+
 git clone https://github.com/safesploit/doogle.git
 mkdir src
 mv ./doogle ./src/
@@ -27,6 +36,13 @@ catch(PDOExeption $e)
 }
 ?>
 ' > config.php
-mv ./config.php ./src/doogle/config.php 
+
+mv ./config.php ./src/doogle/config.php
+
 
 docker-compose up -d --build
+# docker compose build
+
+echo 'Run: $ docker compose up'
+
+exit 1
